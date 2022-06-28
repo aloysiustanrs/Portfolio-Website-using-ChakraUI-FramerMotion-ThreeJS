1e21c6e280cb319d4e838968a63d37c0bf2195b9
aloysiustanrs <aloysiustanrs@gmail.com>
2022-06-28 14:22:21 +0800

    complete all crypto website blog

--- components/blog/Image.js
+++ components/blog/Image.js
@@ -2,11 +2,11 @@ import React from "react";
 import Zoom from "react-medium-image-zoom";
 import "react-medium-image-zoom/dist/styles.css";
 
-const Image = ({ alt, src }) => {
+const Image = ({ alt, src, width }) => {
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
       <Zoom>
-        <img alt={alt} src={src} width="600" />
+        <img alt={alt} src={src} width={width || "600"} />
       </Zoom>
     </div>
   );
